3c40212095c31083c25acd9d613325ddaee58eeb
Shuangzhen <dreamre21@gmail.com>
2020-08-08 12:00:10 +0800

    Leetcode402_移除K数字 - 2020年 8月 8日 星期六 12时00分10秒 CST

--- 7_贪心/Leetcode_402_RemoveKDigits.cpp
+++ 7_贪心/Leetcode_402_RemoveKDigits.cpp
@@ -7,28 +7,36 @@
  *      已知一个使用字符串的非负整数，求移除K个数字后，获得最小的可能新数字
  *
  *      从高位向低位遍历，如果对应数字大于下一位数字，将该数字去掉
- *      1. 所有数字扫描完成 k 仍大于0，比如 12345 k =3
+ *
+ *      注意：
+ *      1. 所有数字扫描完成 k 仍大于0，比如 12345 k =3   依次出栈
  *      2. 当数字有0 比如 100200, k=1
  *      3. 返回字符串
  *
  */
 #include <string>
 #include <vector>
-using namespace std;
+#include <iostream>
+
+using std::string;
+using std::vector;
 
 class Solution {
 public:
     string removeKdigits(string num, int k) {
         vector<int> S;
-        string res = "";
+        string res{};
 
         for (int i=0; i < num.length(); i++){
             int number = num[i] - '0';
-            while (S.size() != 0 && S[S.size()-1] > num[i] && k > 0){
+
+            // 新数字 > 栈中数字，弹出
+            while (S.size() != 0 && S[S.size()-1] > number && k > 0){
                 S.pop_back();
                 k--;
             }
-            if (k == 0){
+            // 入栈
+            if (number != 0 || S.size() != 0){
                 S.push_back(number);
             }
         }
@@ -39,7 +47,7 @@ public:
         }
 
         for (int i=0; i < S.size(); i++){
-            res.insert(S[i]);
+            res.append(1, '0'+S[i]);
         }
 
         if (res == ""){
@@ -52,6 +60,14 @@ public:
 
 
 int main(){
+    string num{"10002000"};
+    int k = 3;
+
+    Solution sol;
+    string res = sol.removeKdigits(num, k);
+
+    std::cout << res << std::endl;
+//    assert(res == "12");
 
     return 0;
 }
