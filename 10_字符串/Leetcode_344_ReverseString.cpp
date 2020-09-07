38282e0a1f4734333256cdbdcc9bb32ef0dd3ece
Shuangzhen <dreamre21@gmail.com>
2020-09-08 07:30:44 +0800

    Leetcode334 字符串反转 - 2020年 9月 8日 星期二 07时30分44秒 CST

---
+++ 10_字符串/Leetcode_344_ReverseString.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Yang Shuangzhen on 2020/9/8.
+//
+
+#include <vector>
+
+
+class Solution {
+public:
+    void reverseString(std::vector<char> &s) {
+        for (int i=0, j=s.size()-1; i < s.size()/2; i++, j--){
+            std::swap(s[i], s[j]);
+        }
+    }
+};
+
+int main(){
+    std::vector<char> s = {'h', 'e', 'l', 'l', 'o'};
+    std::vector<char> rs = {'o', 'l', 'l', 'e', 'h'};
+
+    Solution sol;
+    sol.reverseString(s);
+    assert(s == rs);
+
+    return 0;
+}
+
