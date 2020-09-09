a94163708854c2e1ceba380078c8fd80cf4112b9
Shuangzhen <dreamre21@gmail.com>
2020-09-09 20:29:53 +0800

    二分思路：数组中无target返回high - 2020年 9月 9日 星期三 20时29分53秒 CST

--- 11_数组/Leetcode_35_SearchInsertPos.cpp
+++ 11_数组/Leetcode_35_SearchInsertPos.cpp
@@ -5,7 +5,11 @@
 #include <vector>
 
 /*
- *
+ *    Leetcode 35: 搜索插入位置
+ *    基本思路：
+ *       解法1：遍历数组，当 target >= nums[i] 返回i
+ *       解法2: 二分查找
+ *              二分查找数组中的元素，返回元素索引，若target不在数组中，二分查找结束，返回high
  */
 
 class Solution {
@@ -34,7 +38,7 @@ int main(){
     std::vector<int> nums({2, 3, 5, 6, 9, 10});
 
     Solution sol;
-    assert(sol.searchInsert(nums, 5) == 2);
+    assert(sol.searchInsert(nums, 11) == 6);
 
     return 0;
 }
